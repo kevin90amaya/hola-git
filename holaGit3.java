6609dddbe027ed642587ac0ee79c24d5f391b8b7
kevin <kevin90amaya@gmail.com>
2026-01-27 15:50:47 -0300

    modifique el holaGit3 , pero no tenia los camvios que se abian echo en este archivo en la rama login

--- holaGit3.java
+++ holaGit3.java
@@ -1,6 +1,6 @@
 public class holaGit3 {
     
     public static void main(String[] args) {
-        System.out.println("Hola Git 3 modificado despues de crear el branch login");
+        System.out.println("Hola Git 3 modificado despues de crear el branch login y no aparece la modificacion que se le iso a este archibo desde la rama login");
     }
 }
