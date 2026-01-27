8a2d50247a0c5bc605e0146e2bf9721b85f8b184
kevin <kevin90amaya@gmail.com>
2026-01-27 15:44:29 -0300

    voy a simular un conflicto al aser un brench

--- holaGit3.java
+++ holaGit3.java
@@ -1,6 +1,6 @@
 public class holaGit3 {
     
     public static void main(String[] args) {
-        System.out.println("Hola Git 3 modificado despues de crear el branch login");
+        System.out.println("Hola Git 3 modificado despues de crear el branch login y despues de hacer el merge esto me va a dar error ");
     }
 }
