99c9b6fdfa4aaa6b260b8238fe619c4eaaba1d50
kevin <kevin90amaya@gmail.com>
2026-01-28 13:30:47 -0300

    se elimino la rama login

--- login.java
+++ login.java
@@ -1,6 +1,6 @@
 public class login {
     
     public static void main(String[] args) {
-        System.out.println("Login exitoso modificado");
+        System.out.println("Login terminado");
     }
 }
